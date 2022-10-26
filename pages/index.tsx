a75e5360c003f4e8e32b5b491be1156068d8eb8c
tsubasa_nishikawa <tsubasa.nishikawa@jp.ricoh.com>
2022-10-26 16:44:30 +0900

    グループ分け結果の表のデザイン変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,8 +38,8 @@ const Home: NextPage = () => {
         </div>
 
         <div>
-          <table>
-              <thead>
+          <table border = {1}>
+              <thead className={styles.tableHead}>
                 <tr><th>1</th><th>2</th><th>3</th></tr>
               </thead>
 
