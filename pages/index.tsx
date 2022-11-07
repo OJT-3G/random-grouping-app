d622df81ea4e322307c51e3ab4551a6b7bf4a852
tsubasa-nishikawa <110072048+tsubasa-nishikawa@users.noreply.github.com>
2022-11-07 11:29:33 +0900

    グループ分け結果を表示する(グループ数とグループ分けに制約あり) (#14)
    * メンバーのダミーデータの作成
    
    * ダミーデータのメンバーを手動でグループ分けし表で表示する
    
    * グループ分け結果の表のデザイン変更
    
    * 空行を消す（グループ分け結果の表示の修正）
    
    * スタイルをケバブケースに変更（グループ分け結果の表示の修正）
    
    * 変数をletからconstに変更（グループ分け結果の表示の修正）
    
    * 改行コードを統一したいため「CRLF」から「LF」に変更
    
    * 呼び出し元が書きづらくなった為ケバブケースを元のキャメルケ－スに戻した
    
    * インデントをスペース2つに統一し、テーブルの要素のタグが見やすいように改行した

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,9 @@ const Home: NextPage = () => {
   const [groupNumber, setGroupNumber] = useState(1);
   const [errorMessage, setErrorMessage] = useState('');
 
+  // グループメンバーのダミーデータ
+  const groupMember = ['ふじい','あらかわ','あさい','いいだ','いしざき','なかむら','なかしま','にしかわ','おぎや','おおさか','さんのう','たなか'];
+
   const onChangeTextBox = (event: { target: { value: string; }; }) => {
     const targetValue = event.target.value;
     setGroupNumber(parseInt(targetValue));
@@ -25,15 +28,41 @@ const Home: NextPage = () => {
         <title>random grouping app</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
-
         <div>
           <p className={styles.label}>グループ数</p>
           <p className={styles.errorMessage}>{errorMessage}</p>
           <input type="number" min="1" max="20" value={groupNumber} onChange={onChangeTextBox}/>
         </div>
-
+        <div>
+          <table border = {1}>
+            <thead className={styles.tableHead}>
+              <tr><th>1</th><th>2</th><th>3</th></tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{groupMember[0]}</td>
+                <td>{groupMember[1]}</td>
+                <td>{groupMember[2]}</td>
+              </tr>
+              <tr>
+                <td>{groupMember[3]}</td>
+                <td>{groupMember[4]}</td>
+                <td>{groupMember[5]}</td>
+              </tr>
+              <tr>
+                <td>{groupMember[6]}</td>
+                <td>{groupMember[7]}</td>
+                <td>{groupMember[8]}</td>
+              </tr>
+              <tr>
+                <td>{groupMember[9]}</td>
+                <td>{groupMember[10]}</td>
+                <td>{groupMember[11]}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </main>
     </div>
   )
