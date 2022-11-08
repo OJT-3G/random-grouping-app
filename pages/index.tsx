7b3046af33d0dc3fb1c644ae7cf5808d08208b01
tsubasa-nishikawa <110072048+tsubasa-nishikawa@users.noreply.github.com>
2022-11-08 14:48:44 +0900

    ESLintとPretterを導入し既存コードを整形する (#20)
    * レビューの負荷を下げるためESLintとPrettierの導入
    ルールはテーマに合わせている
    
    * コードの自動整形を出来るようにする(自動整形済み)
    
    * 何のアプリなのか、どのように起動するのか等がわかるようにREADMEの作成を行った
    
    * コマンドを「yarn」に統一し、Prettierでもコードの静的解析を行っている事を追記した

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,36 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useState } from 'react';
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-
-  const [groupNumber, setGroupNumber] = useState(1);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [groupNumber, setGroupNumber] = useState(1)
+  const [errorMessage, setErrorMessage] = useState('')
 
   // グループメンバーのダミーデータ
-  const groupMember = ['ふじい','あらかわ','あさい','いいだ','いしざき','なかむら','なかしま','にしかわ','おぎや','おおさか','さんのう','たなか'];
+  const groupMember = [
+    'ふじい',
+    'あらかわ',
+    'あさい',
+    'いいだ',
+    'いしざき',
+    'なかむら',
+    'なかしま',
+    'にしかわ',
+    'おぎや',
+    'おおさか',
+    'さんのう',
+    'たなか',
+  ]
 
-  const onChangeTextBox = (event: { target: { value: string; }; }) => {
-    const targetValue = event.target.value;
-    setGroupNumber(parseInt(targetValue));
+  const onChangeTextBox = (event: { target: { value: string } }) => {
+    const targetValue = event.target.value
+    setGroupNumber(parseInt(targetValue))
 
     if (targetValue === '') {
-      setErrorMessage('グループ数を指定してください');
+      setErrorMessage('グループ数を指定してください')
     } else {
-      setErrorMessage('');
+      setErrorMessage('')
     }
   }
 
@@ -26,18 +38,28 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <Head>
         <title>random grouping app</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <main className={styles.main}>
         <div>
           <p className={styles.label}>グループ数</p>
           <p className={styles.errorMessage}>{errorMessage}</p>
-          <input type="number" min="1" max="20" value={groupNumber} onChange={onChangeTextBox}/>
+          <input
+            type='number'
+            min='1'
+            max='20'
+            value={groupNumber}
+            onChange={onChangeTextBox}
+          />
         </div>
         <div>
-          <table border = {1}>
+          <table border={1}>
             <thead className={styles.tableHead}>
-              <tr><th>1</th><th>2</th><th>3</th></tr>
+              <tr>
+                <th>1</th>
+                <th>2</th>
+                <th>3</th>
+              </tr>
             </thead>
             <tbody>
               <tr>
